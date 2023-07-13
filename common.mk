396369b150856fd2990d3dfc609bc549f71eaf4f
chandu078 <chandudyavanapelli03@gmail.com>
2023-07-13 07:15:40 +0200

    sm8450-common: Update IMS from T1TBS33.32-8-9-7-2
    * Match with LA.QSSI.13.0.r1.11.00.00.913.072.01 QSSI tag.
    
    Change-Id: I84c3e4e856ef2d4dd7dcf2d58528b4b2475f4f99
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: rk134 <rahul@aospa.co>

--- common.mk
+++ common.mk
@@ -418,7 +418,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@2.0-service.qti-v2
 
 # Touch
 PRODUCT_PACKAGES += \
